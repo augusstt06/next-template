76dc7ffbd965e4f5fef6a79507d5aebd10be3b16
augusstt06 <augusstt06@gmail.com>
2024-10-13 18:00:40 +0900

    fix: layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
-'use client'
-
 import React from 'react'
 
-import { ThemeProvider } from 'next-themes'
+// import { ThemeProvider } from 'next-themes'
 
 export default function RootLayout({
   children,
@@ -12,7 +10,8 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning={true}>
       <body>
-        <ThemeProvider attribute="class">{children}</ThemeProvider>
+        {/* <ThemeProvider attribute="class">{children}</ThemeProvider> */}
+        {children}
       </body>
     </html>
   )
